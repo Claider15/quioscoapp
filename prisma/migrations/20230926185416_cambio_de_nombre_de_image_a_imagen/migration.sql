ca03fe021b50d754d905400b8c717b338d10fc3e
Claider Arturo Peña Moreno <claiderarturo@hotmail.com>
2023-10-04 12:50:01 -0600

    primer deployment

--- prisma/migrations/20230926185416_cambio_de_nombre_de_image_a_imagen/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `image` on the `producto` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Producto` DROP COLUMN `image`,
-    ADD COLUMN `imagen` VARCHAR(191) NULL;
